f52f31686d65cf8a1a8d9e51dd0ed1df625f62bf
csy <277376753@qq.com>
2019-05-04 16:10:37 +0800

    add row and col

---
+++ src/col.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "col"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "row"
+    }
+</script>
+
+<style scoped>
+
+</style>
