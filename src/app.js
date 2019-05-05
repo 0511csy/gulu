cd58de59b619061401afb46a2df2fafd6c13e7c5
csy <277376753@qq.com>
2019-05-05 21:24:57 +0800

    基本row和col

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
+Vue.component('g-row',Row)
+Vue.component('g-col',Col)
 
 new Vue({
     el:'#app',
@@ -18,12 +22,7 @@ new Vue({
         message:''
     },
     created(){
-        setTimeout(()=>{
-            let event = new Event('change')
-            let inputElement = this.$el.querySelector('input')
-            inputElement.dispatchEvent(event)
-            console.log('hi')
-        },3000)
+
     },
     methods:{
         inputChange(e){
