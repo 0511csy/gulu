121bbf35a7d11cbfc01dfb12cf2c4c3f6405428a
csy <277376753@qq.com>
2019-05-05 22:26:10 +0800

    重构row和col

--- src/col.vue
+++ src/col.vue
@@ -1,48 +1,58 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-         :style="{paddingLeft: gutter/2+'px',paddingRight:gutter/2+'px'}" >
-        <div style="border:1px solid green; height: 100px">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass"
+         :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
         name: "GuluCol",
-        props:{
-            span:{
-                type:[Number,String]
+        props: {
+            span: {
+                type: [Number, String]
             },
-            offset:{
-                type:[Number,String]
+            offset: {
+                type: [Number, String]
             },
-            gutter:{
-                type:[Number,String]
+            gutter: {
+                type: [Number, String]
             }
         },
-        data(){
-            return{
-                gutter:0
+
+        computed: {
+            colClass(){
+                let {span,offset} = this
+                return [
+                    span && `col-${span}`,
+                    offset && `offset-${offset}`
+                ]
+            },
+            colStyle() {
+                let {gutter} = this
+                return {
+                    paddingLeft: gutter / 2 + 'px',
+                    paddingRight: gutter / 2 + 'px'
+                }
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-    .col{
+    .col {
         height: 100px;
         width: 50%;
-        $class-prefix:col-;
-        @for $n from 1 through 24{
-            &.#{$class-prefix}#{$n}{
-                width:($n/24)*100%;
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n/24)*100%;
             }
         }
-        $class-prefix:offset-;
-        @for $n from 1 through 24{
-            &.#{$class-prefix}#{$n}{
-                margin-left:($n/24)*100%;
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
