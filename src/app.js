640594c7b4e90aa069b43a8fb700d1fbb846a150
csy <277376753@qq.com>
2019-05-13 00:01:46 +0800

    加入动画

--- src/app.js
+++ src/app.js
@@ -40,9 +40,18 @@ new Vue({
 
     },
     methods:{
-        showToast(){
+        showToast1(){
+            this.showToast('top')
+        },
+        showToast2(){
+            this.showToast('middle')
+        },
+        showToast3(){
+            this.showToast('bottom')
+        },
+        showToast(position){
             this.$toast(`你的当前智商为 ${parseInt(Math.random() * 100)},你的智商需要充值`,{
-                position:'bottom',
+                position,
                 enableHtml:false,
                 closeButton:{
                     text:'已充值',
