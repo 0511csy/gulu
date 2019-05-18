163245caf51338aec060ab8394a0c2280004f2ea
csy <277376753@qq.com>
2019-05-18 14:59:04 +0800

    tabs

---
+++ src/tabs-item.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="tabs-item" :class="classes" @click="onClick">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GuluTabsItem",
+        inject:['eventBus'],
+        data(){
+            return{
+                active:false
+            }
+        },
+        props:{
+
+            disabled:{
+                type: Boolean,
+                default: false
+            },
+            name:{
+                type: String | Number,
+                required: true
+            }
+        },
+        computed:{
+          classes(){
+              return{
+                  active:this.active,
+                  disabled:this.disabled
+              }
+          }
+        },
+        created(){
+            this.eventBus.$on('update:selected',(name)=>{
+                this.active = name === this.name;
+            })
+        },
+        methods:{
+            onClick(){
+                if(this.disabled){return}
+                this.eventBus.$emit('update:selected',this.name,this)
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    $disabled-text-color: grey;
+    $blue: blue;
+.tabs-item{
+    flex-shrink: 0;
+    padding: 0 1em;
+    cursor:pointer;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    &.active{
+        color: $blue;
+        font-weight: bold;
+    }
+    &.disabled{
+        color: $disabled-text-color;
+        cursor: not-allowed;
+    }
+}
+</style>
