79529097474ef16d5f792608ea7918e17d931f94
csy <277376753@qq.com>
2019-05-19 23:07:06 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper"  v-if="visible"  >
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" style="display:inline-block">
             <slot></slot>
         </span>
 
@@ -22,6 +22,8 @@
             onclickDocument (e) {
                 if(this.$refs.popover &&
                     (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))){return}
+                if(this.$refs.contentWrapper &&
+                    (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))){return}
                 this.close()
             },
             positionContent(){
@@ -55,6 +57,8 @@
 </script>
 
 <style scoped lang="scss">
+    $border-color: #333;
+    $border-radius: 4px;
 .popover{
     display: inline-block;
     vertical-align: top;
@@ -64,8 +68,32 @@
 .content-wrapper{
     position: absolute;
     left: 0;
-    border: 1px solid #000;
-    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border: 1px solid $border-color;
+    filter:drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
     transform:translateY(-100%);
+    background: #ffffff;
+    border-radius: $border-radius;
+    margin-top: -10px;
+    padding: .5em 1em;
+    word-break:break-all;
+    max-width: 20em;
+    &::before,&::after {
+        content:'';
+        display: block;
+        border: 10px solid transparent;
+        border-top-color: #000;
+        position: absolute;
+        width: 0;
+        height: 0;
+        top: 100%;
+        left: 10px;
+    }
+    &::before{
+        border-top-color: #000;
+    }
+    &::after {
+        border-top-color: #ffffff;
+        top: calc(100% - 1px);
+    }
 }
 </style>
